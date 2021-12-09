6f1247992c3eae90a5b21bdcb69c714819ddf4e6
appleseed-iii <appleseed-iii@protonmail.com>
2021-12-09 08:31:00 -0600

    mui setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,39 @@
-import '../styles/globals.css'
 import { GlobalProvider } from '../context/AppContext';
 import type { AppProps } from 'next/app'
 
-function Application({ Component, pageProps }: AppProps) {
+import * as React from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from '../styles/theme';
+import createEmotionCache from '../utils/createEmotionCache';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface ConcaveAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+function ConcaveApp(props: ConcaveAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
   return (
-    <GlobalProvider>
-      <Component {...pageProps} />
-    </GlobalProvider>
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Change title in _app.tsx</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <GlobalProvider>
+          <Component {...pageProps} />
+        </GlobalProvider>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
-export default Application
+export default ConcaveApp
